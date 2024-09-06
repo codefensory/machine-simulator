28edbce8c84425ee9be847e47d105d67551a4756
Yeferson <yefer.el.teke@gmail.com>
2024-09-05 23:41:14 -0500

    chore add all videos to memory

--- app/src/steps/StepsManager.jsx
+++ app/src/steps/StepsManager.jsx
@@ -48,51 +48,40 @@ export const StepsManager = forwardRef(
 
     return (
       <>
-        {canShow('ESPERA_LOOP') && (
-          <VideoTemplate
-            state="ESPERA_LOOP"
-            onPointerDown={handleStart}
-            onPointerUp={handleStop}
-            onPlaying={onPlaying}
-            src="1-inicio.mp4"
-            muted
-            loop
-            hidden={currentState !== 'ESPERA_LOOP'}
-          />
-        )}
-        {(currentState === 'ESPERA_LOOP' ||
-          currentState === 'EXPLICACION' ||
-          currentState === 'ESPERA_BOTON_INICIO') && (
-          <VideoTemplate
-            state="EXPLICACION"
-            onEnded={onConfirm(false)}
-            onClick={onConfirm(true)}
-            onPlaying={onPlaying}
-            muted
-            src="2-explicacion.mp4"
-            hidden={
-              currentState !== 'EXPLICACION' &&
-              currentState !== 'ESPERA_BOTON_INICIO'
-            }
-          />
-        )}
+        <VideoTemplate
+          state="ESPERA_LOOP"
+          onPointerDown={handleStart}
+          onPointerUp={handleStop}
+          onPlaying={onPlaying}
+          src="1-inicio.mp4"
+          muted
+          loop
+          hidden={currentState !== 'ESPERA_LOOP'}
+        />
+        <VideoTemplate
+          state="EXPLICACION"
+          onEnded={onConfirm(false)}
+          onClick={onConfirm(true)}
+          onPlaying={onPlaying}
+          muted
+          src="2-explicacion.mp4"
+          hidden={
+            currentState !== 'EXPLICACION' &&
+            currentState !== 'ESPERA_BOTON_INICIO'
+          }
+        />
         {currentState === 'ESPERA_BOTON_INICIO' && (
           <Modal onClick={onConfirm(false)} />
         )}
-        {(currentState === 'ESPERA_BOTON_INICIO' ||
-          currentState === 'ELEGIR_FONDO' ||
-          currentState === 'TUTORIAL') && (
-          <VideoTemplate
-            state="TUTORIAL"
-            muted
-            onClick={onConfirm(true)}
-            onEnded={onConfirm(false)}
-            onPlaying={onPlaying}
-            src="3-tutorial.mp4"
-            hidden={currentState !== 'TUTORIAL'}
-          />
-        )}
-
+        <VideoTemplate
+          state="TUTORIAL"
+          muted
+          onClick={onConfirm(true)}
+          onEnded={onConfirm(false)}
+          onPlaying={onPlaying}
+          src="3-tutorial.mp4"
+          hidden={currentState !== 'TUTORIAL'}
+        />
         <video
           ref={videoRef}
           autoPlay
@@ -112,75 +101,57 @@ export const StepsManager = forwardRef(
             showTuto={currentState === 'MOVIMIENTO_EXCAVADORA'}
           />
         )}
-
         {currentState === 'TERMINADO' && (
           <img
             src="/assets/backgrounds/carga_exitosa.png"
             className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[560px] h-auto"
           />
         )}
-
-        {(currentState === 'MOVIMIENTO_EXCAVADORA' ||
-          currentState === 'CIBER_ATAQUE') && (
-          <VideoTemplate
-            state="CIBER_ATAQUE"
-            onEnded={onConfirm(false)}
-            onClick={onConfirm(true)}
-            muted
-            src="4-cyber-ataque.mp4"
-            onPlaying={onPlaying}
-            hidden={currentState !== 'CIBER_ATAQUE'}
-          />
-        )}
-        {(currentState === 'CIBER_ATAQUE' ||
-          currentState === 'ESPERA_ACTIVAR_LIMPIEZA') && (
-          <VideoTemplate
-            state="ESPERA_ACTIVAR_LIMPIEZA"
-            onClick={onConfirm(false)}
-            muted
-            loop
-            src="5-espera-analizar.mp4"
-            onPlaying={onPlaying}
-            hidden={currentState !== 'ESPERA_ACTIVAR_LIMPIEZA'}
-          />
-        )}
-        {(currentState === 'ESPERA_ACTIVAR_LIMPIEZA' ||
-          currentState === 'LIMPIEZA') && (
-          <VideoTemplate
-            state="LIMPIEZA"
-            muted
-            onClick={onConfirm(true)}
-            onEnded={onConfirm(false)}
-            src="6-analizando.mp4"
-            onPlaying={onPlaying}
-            hidden={currentState !== 'LIMPIEZA'}
-          />
-        )}
-        {(currentState === 'LIMPIEZA' || currentState === 'ESPERA_RETOMAR') && (
-          <VideoTemplate
-            state="ESPERA_RETOMAR"
-            muted
-            onClick={onConfirm(false)}
-            loop
-            src="7-espera-retomar.mp4"
-            onPlaying={onPlaying}
-            hidden={currentState !== 'ESPERA_RETOMAR'}
-          />
-        )}
-
-        {(currentState === 'MOVIMIENTO_EXCAVADORA_FINAL' ||
-          currentState === 'TERMINADO' ||
-          currentState === 'AGRADECIMIENTO') && (
-          <VideoTemplate
-            state="AGRADECIMIENTO"
-            muted
-            onEnded={onConfirm(false)}
-            onClick={onConfirm(true)}
-            src="8-agradecimiento.mp4"
-            onPlaying={onPlaying}
-            hidden={currentState !== 'AGRADECIMIENTO'}
-          />
-        )}
+        <VideoTemplate
+          state="CIBER_ATAQUE"
+          onEnded={onConfirm(false)}
+          onClick={onConfirm(true)}
+          muted
+          src="4-cyber-ataque.mp4"
+          onPlaying={onPlaying}
+          hidden={currentState !== 'CIBER_ATAQUE'}
+        />
+        <VideoTemplate
+          state="ESPERA_ACTIVAR_LIMPIEZA"
+          onClick={onConfirm(false)}
+          muted
+          loop
+          src="5-espera-analizar.mp4"
+          onPlaying={onPlaying}
+          hidden={currentState !== 'ESPERA_ACTIVAR_LIMPIEZA'}
+        />
+        <VideoTemplate
+          state="LIMPIEZA"
+          muted
+          onClick={onConfirm(true)}
+          onEnded={onConfirm(false)}
+          src="6-analizando.mp4"
+          onPlaying={onPlaying}
+          hidden={currentState !== 'LIMPIEZA'}
+        />
+        <VideoTemplate
+          state="ESPERA_RETOMAR"
+          muted
+          onClick={onConfirm(false)}
+          loop
+          src="7-espera-retomar.mp4"
+          onPlaying={onPlaying}
+          hidden={currentState !== 'ESPERA_RETOMAR'}
+        />
+        <VideoTemplate
+          state="AGRADECIMIENTO"
+          muted
+          onEnded={onConfirm(false)}
+          onClick={onConfirm(true)}
+          src="8-agradecimiento.mp4"
+          onPlaying={onPlaying}
+          hidden={currentState !== 'AGRADECIMIENTO'}
+        />
       </>
     );
   }
